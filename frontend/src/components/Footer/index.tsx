ed4118df1a22a39a5e1d266fb3a33a6ffa34cf8d
Victoria <victoriabotelho14@gmail.com>
2021-05-08 10:39:16 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ImgDsDark from 'assets/img/ds-dark.svg';
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/vyk1" target="_blank" rel="noreferrer">Victoria Botelho Martins</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior.
+      <br />
+                    <a href="https://www.linkedin.com/in/victoria-botelho-martins-046a5b153/" target="_blank" rel="noreferrer">LinkedIn</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
